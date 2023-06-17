75b0494be25b87a7b670507ce32e6c72d9550e14
max <hennighausenmax@gmail.com>
2023-06-17 21:28:19 +0200

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const appRoute = require("./routes/apps");
+var cors = require('cors')
+
+
+dotenv.config();
+const url = `mongodb+srv://max:Password123@cluster0.owcfc.mongodb.net/react01?retryWrites=true&w=majority`
+const connectionParams={
+    useNewUrlParser: true,
+//    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(url, connectionParams)
+    .then(()=>{
+    console.log("Connected to MongoDB")
+    })
+    .catch((err)=> {
+        console.error(`Error connecting to database.n${err}`);
+    })
+
+//middleware
+//const corsOptions = { 
+    // origin:'https://abc.onrender.com',
+//    AccessControlAllowOrigin: '*',  
+//    origin: '*',  
+//    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' 
+//  }
+//app.use(cors(corsOptions))
+
+app.use(cors())
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("comman"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/apps", appRoute);
+
+app.get("/api", (req, res) => {
+    res.json({ "users": ["userOne", "userTwo", "userThree"] })
+})
+
+app.listen(8800,()=>{
+    console.log("Backen server is running change")
+})
